b90ae415b78673671625108aa0db9282a14be3ce
katherineti <katherineti1609@gmail.com>
2025-02-13 19:31:16 -0400

    con el ORM drizzle se hace conexion a PostgreSQL al servidor del curso y Bd kat-db. Se crea tabla usersTable con PK y FK, tabla roleTable

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { integer, pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const roleTable = pgTable("roles",{
+  id: serial().primaryKey(),
+  code: varchar({ length: 255 }).notNull().unique() ,
+})
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  lastname: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  created_at: timestamp(). defaultNow(),
+  roles_id: integer().notNull().references( ()=>roleTable.id )
+});
